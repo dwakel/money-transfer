cb9873809b5dd5dcef629cd1bf9f27553baf503e
dwakel <selorm.avoke@cowtribe.com>
2020-11-08 23:45:00 +0000

    User controller and Service

---
+++ src/main/java/test/moneytransfer/api/UserController.java
@@ -0,0 +1,37 @@
+package test.moneytransfer.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import test.moneytransfer.model.User;
+import test.moneytransfer.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/user")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User AddUser(@RequestBody User user){
+        return this.userService.AddUser(user);
+    }
+
+    @GetMapping(path = "{id}")
+    public User GetUserById(@PathVariable("id") UUID id) {
+        return this.userService.GetUser(id);
+    }
+
+    @PostMapping(path = "{id} ")
+    public User UpdateUser(@PathVariable("id")UUID id, @RequestBody User user){
+        return this.userService.UpdateUser(id, user);
+    }
+}
---
+++ src/main/java/test/moneytransfer/service/UserService.java
@@ -0,0 +1,28 @@
+package test.moneytransfer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import test.moneytransfer.data.IUserRepository;
+import test.moneytransfer.model.User;
+
+import java.util.UUID;
+
+public class UserService {
+    private final IUserRepository userRepository;
+
+    @Autowired
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User AddUser(User user){
+        return userRepository.createUser(UUID.randomUUID(), user);
+    }
+
+    public User GetUser(UUID userId){
+        return userRepository.findUserById(userId).orElse(null);
+    }
+
+    public User UpdateUser(UUID userId, User user){
+        return userRepository.updateUserById(userId, user);
+    }
+}
